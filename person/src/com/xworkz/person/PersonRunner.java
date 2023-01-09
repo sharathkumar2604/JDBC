4d863cadb22f1b0543065ad5fcbac0134c90d4f6
sharathkumar2604 <sharathkumargs.xworkz@gmail.com>
2023-01-09 11:22:27 +0530

    jdbc

--- person/src/com/xworkz/person/PersonRunner.java
+++ person/src/com/xworkz/person/PersonRunner.java
@@ -9,6 +9,7 @@ public class PersonRunner {
 public static void main(String[] args) {
 	
 
+	
 	String username="root";
 	String password="Sathish@123@";
 	String host="jdbc:mysql://localhost:3306/college";
@@ -24,10 +25,29 @@ public static void main(String[] args) {
 		PreparedStatement statement = connection.prepareStatement(query);
 		
 		statement.setInt(1, 2);
+		statement.setString(2, "Shreyas");
+		statement.setInt(3, 32);
+		statement.setString(4, "Mandya");
+		statement.setString(5, "Malavalli");
+		
+		statement.setInt(1, 3);
 		statement.setString(2, "Raghu");
+		statement.setInt(3, 29);
+		statement.setString(4, "Mysore");
+		statement.setString(5, "Degree");
+		
+		statement.setInt(1, 4);
+		statement.setString(2, "gopal");
+		statement.setInt(3, 30);
+		statement.setString(4, "Ballary");
+		statement.setString(5, "Bcom");
+		
+		statement.setInt(1, 5);
+		statement.setString(2, "Rakesh");
 		statement.setInt(3, 32);
 		statement.setString(4, "Gadag");
-		statement.setString(5, "Degree");
+		statement.setString(5, "Diplomo");
+		
 		
 		statement.executeUpdate();
 		
